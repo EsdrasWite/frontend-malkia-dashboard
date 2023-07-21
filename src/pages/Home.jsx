2634263216b01f414ee3703499d4d5b004951090
esdraswite <witeesdras@gmail.com>
2023-07-21 16:19:09 +0200

    add chart to the grid container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
                 <Card type="niveauEau" />
             </Box>
 
-            <div className='chart'>
+            {/* <div className='chart'>
                 <div className="lefChat">
                     <div className="legend">
                         <span className="legendLabel">Humidité du sol</span>
@@ -30,11 +30,11 @@ function Home() {
                     <ChartOne />
                 </div>
                 <div className="RightChat">
-                <div className="legend">
+                    <div className="legend">
                         <span className="legendLabel">Humidité ambiante</span>
                         <span className="LegendDot"></span>
                     </div>
-                <ChartTwo />
+                    <ChartTwo />
 
                 </div>
             </div>
@@ -43,16 +43,44 @@ function Home() {
                     <ChartThree />
                 </div>
                 <div className="RightChat">
-                <ChartFour />
+                    <ChartFour />
 
                 </div>
-            </div>
+            </div> */}
 
             <div className="chart2">
-                <div className="box box1">box1</div>
-                <div className="box box2">box2</div>
-                <div className="box box3">bo3x</div>
-                <div className="box box4">box4</div>
+
+                <div className="box box1">
+                    <div className="legend">
+                        <span className="legendLabel">Humidité du sol</span>
+                        <span className="LegendDot"></span>
+                    </div>
+                    <ChartOne />
+                </div>
+
+                <div className="box box2">
+                    <div className="legend">
+                        <span className="legendLabel">Humidité ambiante</span>
+                        <span className="LegendDot"></span>
+                    </div>
+                    <ChartTwo />
+                </div>
+
+                <div className="box box3">
+                    <div className="legend">
+                        <span className="legendLabel">Température ambiante</span>
+                        <span className="LegendDot"></span>
+                    </div>
+                    <ChartThree />
+                </div>
+
+                <div className="box box4">
+                    <div className="legend">
+                        <span className="legendLabel">Niveau citerne d'eau</span>
+                        <span className="LegendDot"></span>
+                    </div>
+                    <ChartFour />
+                </div>
             </div>
 
         </React.Fragment>
