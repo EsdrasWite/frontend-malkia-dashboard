0e470e113e1aa824e04e41636ec3dde11c870dff
esdraswite <witeesdras@gmail.com>
2023-07-21 15:47:48 +0200

    adding of grid system

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,6 +48,13 @@ function Home() {
                 </div>
             </div>
 
+            <div className="chart2">
+                <div className="box box1">box1</div>
+                <div className="box box2">box2</div>
+                <div className="box box3">bo3x</div>
+                <div className="box box4">box4</div>
+            </div>
+
         </React.Fragment>
     );
 }
