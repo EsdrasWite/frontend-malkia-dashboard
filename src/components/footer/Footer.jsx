b3d2c84c772c07683abf6f8754b43f371ec2bf80
esdraswite <witeesdras@gmail.com>
2023-07-23 21:08:37 +0200

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className="appTitle"></div>
-      <div className="copyright"></div>
+      <div className="appTitle">Designed by W-dynasty</div>
+      <div className="copyright">C 2023</div>
     </div>
   )
 }
