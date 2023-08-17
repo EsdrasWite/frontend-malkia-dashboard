83c2e9192ace5f4abee80a9bbb9ea13ca51de3ce
esdraswite <witeesdras@gmail.com>
2023-08-18 00:35:18 +0200

    home review

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,45 +8,42 @@ import ChartTwo from '../components/chart/ChartTwo.jsx';
 import ChartThree from '../components/chart/ChartThree.jsx';
 import ChartFour from '../components/chart/ChartFour.jsx';
 import Footer from '../components/footer/Footer';
-import BootstrapDialogTitle from '../components/dialog/Notification'
-// import Axios from '../services/caller.service.js';
-// import AlertDialog from '../components/dialog/Notification';
 import Axios from '../services/caller.service';
 
 
 function Home() {
 
     const [dataTable, setDataTable] = React.useState([
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 10, niveauciterne: 0, reading_time: '' },
-      ])
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+        { id: 0, temperatureamb: 0, humiditeeamb: 0, humiditeesol: 0, niveauciterne: 0, reading_time: '' },
+    ])
 
-      React.useEffect(() => {
+    React.useEffect(() => {
 
         setInterval(() => {
-          Axios.get(`/infopotfleur`)
-            .then(response => {
-              setDataTable(response.data.data.slice(-10))
-            })
-            .catch(error => console.log(error))
-    
-        }, 1000);
-    
-      }, [])
-    
+            Axios.get(`/infopotfleur`)
+                .then(response => {
+                    if (response.data.data.length > 11) setDataTable(response.data.data.slice(-10));
+                    else setDataTable(current => ([...current]));
+                })
+                .catch(error => console.log(error))
 
-console.log('Home')
+        }, 1000);
+
+    }, [])
+
+    console.log('Home')
 
     return (
         <React.Fragment>
@@ -78,7 +75,7 @@ console.log('Home')
                         <span className="legendLabel">Humidité du sol</span>
                         <span className="LegendDot"></span>
                     </div>
-                    <ChartOne dataTable={dataTable}/>
+                    <ChartOne dataTable={dataTable} />
                 </div>
 
                 <div className="box box2">
@@ -102,7 +99,7 @@ console.log('Home')
                         <span className="legendLabel">Niveau citerne d'eau</span>
                         <span className="LegendDot"></span>
                     </div>
-                    <ChartFour dataTable={dataTable}/>
+                    <ChartFour dataTable={dataTable} />
                 </div>
             </div>
 
