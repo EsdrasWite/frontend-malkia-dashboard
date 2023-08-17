f730f7db13389a3d85f3920cee303b7bda27f6e9
esdraswite <witeesdras@gmail.com>
2023-08-18 01:33:18 +0200

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
-import {AppBar, Box,Toolbar,Typography,Container} from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Container } from '@mui/material';
 
 import { PowerSettingsNew, Adb } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 function NavBar() {
+
+    const navigate = useNavigate()
+    const handleClose = () => {
+        localStorage.clear();
+        navigate("/")
+    }
+
+
+    const username =((localStorage.getItem('username').split('@')[0]).split('\"'))
+
 
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
-                <Toolbar disableGutters sx={{display:'flex', justifyContent:'space-between'}}>
-                    <Box sx={{display:'flex'}} >
+                <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Box sx={{ display: 'flex' }} >
                         <Adb sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1 }} />
                         <Typography
                             variant="h6"
@@ -29,13 +40,13 @@ function NavBar() {
                             POT DE FLEUR CONNECTE
                         </Typography>
                     </Box>
-                    <Box sx={{display:'flex', alignItems:'center'}} >
-                        <PowerSettingsNew sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Box sx={{ display: 'flex', alignItems: 'center' }} >
+                        <Typography sx={{ mr: '20px', fontSize: "12px", color: '#fff', padding: "2px 10px", borderRadius: '3px', backgroundColor: "#38296cc0" }}>{username}</Typography>
+                        <PowerSettingsNew sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, cursor: 'pointer' }} onClick={handleClose} />
                         <Typography
-                            variant="h6"
                             noWrap
                             component="a"
-                            href="/"
+                            onClick={handleClose}
                             sx={{
                                 mr: 2,
                                 display: { xs: 'none', md: 'flex' },
